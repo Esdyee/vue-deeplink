f5b6af59932cffea47777dac5ce3becc34e710be
제일 <joimh7@gmail.com>
2024-02-01 15:07:15 +0900

    tailwindcss 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,ts}"],
+  content: [
+      "./index.html",
+      "./src/**/*.{html,js,ts,vue}",
+      "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
